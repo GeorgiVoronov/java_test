f43fb1cfc3316003bd5fa5e76e5006e7919962b4
Georgi Voronov <georgi.voronov@outlook.com>
2017-02-21 21:06:42 +0200

    area() functions created.

--- sandbox/src/main/java/com/gosch/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/gosch/sandbox/MyFirstProgram.java
@@ -3,12 +3,33 @@ package com.gosch.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		hello("world");
+		hello("user");
+
+		double side = 5;
+      System.out.println("Area of square with side " + side + " = " + area(side));
+
+      double a = 5;
+      double b = 7;
+      System.out.println("Area of rectangle with sides " + a + " and " + b + " = " + area(a,b));
+
       System.out.println(distance(new Point(2,3), new Point(2,3)));
    }
 
 	public static double distance(Point p1, Point p2) {
       return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+   }
+
+   public static void hello(String somebody) {
+      System.out.println("Hello, " + somebody + "!");
+   }
+
+   public static double area(double side) {
+	   return side * side;
+   }
+
+   public static double area(double sideA, double sideB) {
+	   return sideA * sideB;
    }
 	
 }
