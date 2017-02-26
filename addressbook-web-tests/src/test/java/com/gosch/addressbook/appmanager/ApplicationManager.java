e0e4edae5a608eae2a5a25546f09f43a6564d622
Georgi Voronov <georgi.voronov@outlook.com>
2017-02-26 13:51:55 +0200

    Refactoring: Stage3 Inheritance: BaseHelper.

--- addressbook-web-tests/src/test/java/com/gosch/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/gosch/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package com.gosch.addressbook.appmanager;
 
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -13,15 +12,6 @@ public class ApplicationManager {
    private NavigationHelper navigationHelper;
    private GroupHelper groupHelper;
    private ContactHelper contactHelper;
-
-   public static boolean isAlertPresent(FirefoxDriver wd) {
-      try {
-         wd.switchTo().alert();
-         return true;
-      } catch (NoAlertPresentException e) {
-         return false;
-      }
-   }
 
    public void init() {
       System.setProperty("webdriver.gecko.driver", "C:\\Users\\Georgi\\Documents\\Firefox Driver\\geckodriver.exe");
--- addressbook-web-tests/src/test/java/com/gosch/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/com/gosch/addressbook/appmanager/ContactHelper.java
@@ -4,43 +4,31 @@ import com.gosch.addressbook.models.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactHelper {
-
-   private FirefoxDriver wd;
+public class ContactHelper extends BaseHelper {
 
    public ContactHelper(FirefoxDriver wd) {
-      this.wd = wd;
+      super(wd);
    }
 
    public void fillContactForm(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-      wd.findElement(By.name("nickname")).click();
-      wd.findElement(By.name("nickname")).clear();
-      wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+      type(By.name("firstname"), contactData.getFirstName());
+      type(By.name("lastname"), contactData.getLastName());
+      type(By.name("nickname"), contactData.getNickName());
+      type(By.name("mobile"), contactData.getMobile());
+      type(By.name("email"), contactData.getEmail());
+      click(By.xpath("//div[@id='content']/form/input[21]"));
    }
 
    public void returnToHomePage() {
-      wd.findElement(By.linkText("home page")).click();
+      click(By.linkText("home page"));
    }
 
    public void submitContactCreation() {
-      wd.findElement(By.name("submit")).click();
+      click(By.name("submit"));
    }
 
    public void initContactCreation() {
-      wd.findElement(By.linkText("add new")).click();
+      click(By.linkText("add new"));
    }
 
 }
