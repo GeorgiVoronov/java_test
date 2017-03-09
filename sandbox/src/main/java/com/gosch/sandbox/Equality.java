7a62f8b743d5107974e966fd6b6320c787a452a0
Georgi Voronov <georgi.voronov@outlook.com>
2017-03-09 20:29:08 +0200

    Added some small examples to Equality

--- sandbox/src/main/java/com/gosch/sandbox/Equality.java
+++ sandbox/src/main/java/com/gosch/sandbox/Equality.java
@@ -3,17 +3,19 @@ package com.gosch.sandbox;
 public class Equality {
 
    public static void main(String[] args) {
-      String s1 = "firefox";
+      String s1 = "firefox 2.0";
       String s2 = new String(s1);
+      String s3 = "firefox " + Math.sqrt(4.0);
 
       // Компилятор оптимизирует код.
       // Все переменные которые ссылаются на одну и ту же литеральную строку будут ссылаться на один и тот же объект.
       // s1 и s2 имеют один и тот же адрес в памяти.
-      String s3 = "firefox";
+      String s4 = "firefox 2.0";
 
       System.out.println(s1 == s2); //reference equality
       System.out.println(s1.equals(s2));
-      System.out.println(s1 == s3);
+      System.out.println(s1.equals(s3));
+      System.out.println(s1 == s4);
    }
 
 }
