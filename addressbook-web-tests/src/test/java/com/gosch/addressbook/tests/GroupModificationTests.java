0efffa602b762677f7520bf4294a4f6044a1d99a
Georgi Voronov <georgi.voronov@outlook.com>
2017-04-11 20:19:45 +0300

    Groups: Compare lists of GroupModification test.

--- addressbook-web-tests/src/test/java/com/gosch/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/com/gosch/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import com.gosch.addressbook.models.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -18,12 +19,18 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("modified1", "test2", "modified3"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"modified1", "test2", "modified3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         //int after = app.getGroupHelper().getGroupCount();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        // Compare without order - Set
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
 
 }
