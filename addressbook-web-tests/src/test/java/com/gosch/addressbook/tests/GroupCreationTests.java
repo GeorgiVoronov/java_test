9282811872d461da519f4806279e7053b86270ea
Georgi Voronov <georgi.voronov@outlook.com>
2017-04-14 22:15:36 +0300

    Groups: Lambda - GroupCreation Test.

--- addressbook-web-tests/src/test/java/com/gosch/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/gosch/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import com.gosch.addressbook.models.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,19 +15,22 @@ public class GroupCreationTests extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         //int before = app.getGroupHelper().getGroupCount();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1", null, null);
+        GroupData group = new GroupData("test2", null, null);
         app.getGroupHelper().createGroup(group);
         //int after = app.getGroupHelper().getGroupCount();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
+        /*
         int max = 0;
         for (GroupData g : after) {
             if (g.getId() > max) {
                 max = g.getId();
             }
         }
-        group.setId(max);
+        */
+        // Lambda - Anonymous function
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
